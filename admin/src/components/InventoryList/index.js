e3ce3a347e22c9485a3c4bf194c72383c3b7a8fc
duc68456 <duc68456@gmail.com>
2025-12-11 22:06:22 +0700

    remove UpdateLocation

--- admin/src/components/InventoryList/index.js
+++ admin/src/components/InventoryList/index.js
@@ -1,5 +1,5 @@
 export { InventoryList } from './InventoryList';
 export { InventoryListHeader } from './InventoryListHeader';
 export { MovementHistoryModal } from './MovementHistoryModal';
-export { UpdateLocationModal } from './UpdateLocationModal';
+// export { UpdateLocationModal } from './UpdateLocationModal';
 export { TransferStockBulkModal } from './TransferStockBulkModal';
